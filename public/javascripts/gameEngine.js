ef0e6d0b4b5035a024a9fe53d404c8b3fd29792e
Anthony Johnson <atj1979@gmail.com>
2018-09-12 09:05:51 -0700

    Successfully load GLTF

--- public/javascripts/gameEngine.js
+++ public/javascripts/gameEngine.js
@@ -3,51 +3,32 @@ var ctrl = require('three-orbit-controls');
 THREE.OrbitControls = ctrl(THREE);
 var GLTFLoader = require('three-gltf-loader');
 
-// if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 var container, controls;
-var camera, scene, renderer, light; 
+var camera, scene, renderer, light, mixer; 
 
 init();
 animate();
 function init() {
-    debugger
     container = document.createElement( 'div' );
     document.body.appendChild( container );
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-    camera.position.set( -1.8, 300.9, 2.7 );
-    // controls.target.set(new THREE.Vector3(0,0,0));
+    camera.position.set( -1.8, 10.9, 2.7 );
 
     controls = new THREE.OrbitControls( camera );
-    // controls.target.set( 0, -0.2, -0.2 );
     controls.update();
-    // envmap
-    // var path = 'textures/cube/Bridge2/';
-    // var format = '.jpg';
-    // var envMap = new THREE.CubeTextureLoader().load( [
-    // 	path + 'posx' + format, path + 'negx' + format,
-    // 	path + 'posy' + format, path + 'negy' + format,
-    // 	path + 'posz' + format, path + 'negz' + format
-    // ] );
+    
     scene = new THREE.Scene();
-    // scene.background = envMap;
     light = new THREE.HemisphereLight( 0xbbbbff, 0x444422, 1 );
-    var helper = new THREE.HemisphereLightHelper( light, 5 );
-    scene.add(helper);
-    light.position.set( 0, 300, 0 );
-    // scene.add( light );
+    scene.add(light);
     // model
     var loader = new GLTFLoader();
     loader.load( './assets/littlest_tokyo/scene.gltf', function ( gltf ) {
-        gltf.scene.traverse( function ( child ) {
-            // if ( child.isMesh ) {
-            // 	child.material.envMap = envMap;
-            // }
-        } );
         window.controls = controls;
         window.camera = camera;
         window.light = light;
-        window.gltf = gltf.scene;
+        window.gltf = gltf;
 
+        mixer = new THREE.AnimationMixer(gltf);
         gltf.scene.scale.setScalar(.01);
         scene.add( gltf.scene );
         var bbox = new THREE.Box3().setFromObject(scene);
@@ -55,12 +36,21 @@ function init() {
     }, undefined, function ( e ) {
         console.error( e );
     } );
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    scene.add( directionalLight );
+    directionalLight.position.set(2,10,2);
+    directionalLight.lookAt(0,0,0);
+
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.gammaOutput = true;
     container.appendChild( renderer.domElement );
     window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener('keypress', () => {
+        mixer.clipAction(gltf.animations[0]).play();
+    });
+
 }
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
